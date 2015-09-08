a5bd8449a4a900a42a2f4d153231992688691a19
alterloki@yandex.ru <alterloki@yandex.ru>
2015-09-08 19:55:18 +0300

    Added tomcat7 deploy

--- src/main/java/ru/jleague13/Jleague2Application.java
+++ src/main/java/ru/jleague13/Jleague2Application.java
@@ -3,6 +3,8 @@ package ru.jleague13;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import ru.jleague13.controller.NewsController;
 
 @SpringBootApplication
 @Controller
-public class Jleague2Application {
+public class Jleague2Application extends SpringBootServletInitializer {
 
     @Autowired
     private NewsController newsController;
@@ -19,6 +21,11 @@ public class Jleague2Application {
     @RequestMapping("/")
     public String index(Model model) {
         return newsController.getNews(model);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Jleague2Application.class);
     }
 
     public static void main(String[] args) {
