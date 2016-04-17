dd0d68c01baa0d5b8b536799ae18d9b01ae3eaff
alterloki@yandex.ru <alterloki@yandex.ru>
2016-04-17 14:46:49 +0300

    Fixed transfer filter an

---
+++ src/main/java/ru/jleague13/security/CustomUserDetailsService.java
@@ -0,0 +1,53 @@
+package ru.jleague13.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.jleague13.entity.User;
+import ru.jleague13.repository.UserDao;
+import sun.plugin.util.UserProfile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ashevenkov 20.03.16 17:14.
+ */
+@Service("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDao userService;
+
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String ssoId)
+            throws UsernameNotFoundException {
+        /*User user = userService.(ssoId);
+        System.out.println("User : "+user);
+        if(user==null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getSsoId(), user.getPassword(),
+                user.getState().equals("Active"), true, true, true, getGrantedAuthorities(user));*/
+        return null;
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+        /*for(UserProfile userProfile : user.getUserProfiles()){
+            System.out.println("UserProfile : "+userProfile);
+            authorities.add(new SimpleGrantedAuthority("ROLE_"+userProfile.getType()));
+        }*/
+        System.out.print("authorities :"+authorities);
+        return authorities;
+    }
+
+}
---
+++ src/main/java/ru/jleague13/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package ru.jleague13.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author ashevenkov 20.03.16 16:54.
+ */
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsService userDetailsService;
+
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .and().formLogin().loginPage("/login")
+                .usernameParameter("ssoId").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+}
